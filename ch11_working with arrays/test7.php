41ef53ccf8ad66d96ff269401b71acbcf1e5eed5
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-01 16:39:46 +0600

    10_using the sizeof() function to control a for loop#ch11

---
+++ ch11_working with arrays/test7.php
@@ -0,0 +1,10 @@
+<?php 
+$scores = array(66,55,77,88,95);
+print("<h1>SCORES</h1>");
+for($i = 0; $i<sizeof($scores); $i=$i+1)
+{
+   // echo $scores[$i]."<br>";
+   print("<p>$scores[$i]</p>");
+}
+
+
