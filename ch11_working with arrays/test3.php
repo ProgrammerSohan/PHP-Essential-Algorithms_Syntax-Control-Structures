764e23c6b64d00b830e52c2c2e80d844837ec7bb
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-01 10:57:20 +0600

    5_displaying array values_part5#ch11

---
+++ ch11_working with arrays/test3.php
@@ -0,0 +1,14 @@
+<?php
+
+//Displaying array values
+$scores = array(66,55,77,88,95);
+
+print("<p style='font-size:38px; font-weight:bold; padding-top:20px; font-style:italic;color:blue;'>SCORE 1: $scores[0]</p>");
+
+print("<h1>SCORE 2: $scores[1]</h1>");
+
+print("<h2>SCORE 3: $scores[2]</h2>");
+
+print("<h3>SCORE 4: $scores[3]</h3>");
+
+print("<h4>SCORE 5: $scores[4]</h4>");
