b5dea691d0ea1bcaa55341d8ea1c192767aad68d
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-01 17:03:15 +0600

    11_summing & averaging the values in an array#ch11

---
+++ ch11_working with arrays/test8.php
@@ -0,0 +1,14 @@
+<?php 
+$scores = array(66,55,77,88,95);
+$sum = 0;
+for($i = 0; $i<sizeof($scores); $i = $i +1)
+{
+    $sum = $sum + $scores[$i];
+}
+$average = $sum/sizeof($scores);
+
+echo $sum;
+echo "<pre/>";
+echo $average;
+
+
