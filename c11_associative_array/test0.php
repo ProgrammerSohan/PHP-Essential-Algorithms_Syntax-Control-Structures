9dcf4dcaaae9fc53a0faf223cf99f6c3362ff33f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-05 06:14:48 +0600

    _01_using a variable to reference the key of an associative array*11

---
+++ c11_associative_array/test0.php
@@ -0,0 +1,29 @@
+<?php
+    /*
+     * --Associative Array= An associative array is indexed with character strings that are used to describe the content of each element much better than a number.
+     ***Example
+     * */
+/*
+    $scores['Exam1'] =90;
+    $scores['Exam2'] =93;
+    $scores['Exam3'] =87;
+    $scores['Essay'] = 78;
+    $scores['Project'] =80;*/
+
+//Using a variable to reference the key of an associative array
+$userList['mike75']="abc123";
+$userList['mary2']="xyz999";
+$userList['chris17']="abc999";
+$userList['sohan84']="xyz123";
+
+print("<p>Your user ID is sohan84 and your password is {$userList['sohan84']}</p>");
+
+
+
+
+
+
+
+
+
+?>
